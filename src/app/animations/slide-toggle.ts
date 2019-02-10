cae4b98fce7eaea21fd130c0ef580491951bf7dd
Ayan Pal <ayangitrepo@gmail.com>
2019-02-10 22:19:49 +0530

    test commit

--- src/app/animations/slide-toggle.ts
+++ src/app/animations/slide-toggle.ts
@@ -9,4 +9,5 @@ import {
 export let slideToggle = trigger('slideToggle', [
   state('void', style({ transform: 'translateX(-20px)' })),
   transition(':enter, :leave', [animate('0.5s')])
+  // adding this comment ina  new branch
 ]);
